551767d94828ea66901c245dcadf0f57dd3debfc
lavishdudja <lavishdudeja@gmail.com>
2016-09-16 22:13:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '58251-98356-01381-99266',
-    'mongoUrl' : 'http://mongodb://lavish:abcdef@ds029486.mlab.com:29486/tm',
+    'mongoUrl' : 'mongodb://lavish:abcdef@ds029486.mlab.com:29486/tm',
     'facebook': {
         clientID: '302944536723584',
         clientSecret: '49c2b60e24730dfab3ed1d5668911aa8',
