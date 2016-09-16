55276dfafcea65a1a60f6ba7de6b77f035952ca9
lavishdudja <lavishdudeja@gmail.com>
2016-09-16 20:22:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var type = require('./routes/')
 
 var app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
