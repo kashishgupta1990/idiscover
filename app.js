d912b6ef78f6cdcc509e680d8b7c01eceb71a2e6
lavishdudja <lavishdudeja@gmail.com>
2016-09-18 14:23:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express')
 var path = require('path')
-
+// var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var passport = require('passport');
 var authenticate = require('./authenticate');
+
+var config = require('./config');
+
+mongoose.connect(config.mongoUrl);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
 
 var routes = require('./routes/index')
 var users = require('./routes/users')
@@ -23,17 +33,18 @@ var type = require('./routes/')
 
 var app = express()
 
-app.set('port', (process.env.PORT || 3000));
+// view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-
+// passport config
 app.use(passport.initialize());
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -50,14 +61,17 @@ app.use('/t7', t7)
 app.use('/t8', t8)
 app.use('/t9', t9)
 
-
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
+// error handlers
 
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500)
@@ -68,7 +82,8 @@ if (app.get('env') === 'development') {
   })
 }
 
-
+// production error handler
+// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error', {
@@ -77,7 +92,7 @@ app.use(function (err, req, res, next) {
   })
 })
 
-
+// Secure traffic only
 app.all('*', function(req, res, next){
     console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
   if (req.secure) {
